b34334736fcaaf591b6f3ac8f0a4636e8cfa67a3
HarryDo29 <128828116+HarryDo29@users.noreply.github.com>
2026-07-20 19:24:17 +0700

    refactor: comment out automatic MongoDB collection initialization logic

--- internal/initianlize/mongodb.go
+++ internal/initianlize/mongodb.go
@@ -6,7 +6,6 @@ import (
 	_ "go-app/internal/schema"
 	"time"
 
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.uber.org/zap"
@@ -43,25 +42,25 @@ func InitMongoDB() {
 	}
 
 	// Tự động kiểm tra và tạo mới các collection nếu chưa tồn tại
-	collections, err := db.ListCollectionNames(ctx, bson.D{})
-	if err == nil {
-		existing := make(map[string]bool)
-		for _, name := range collections {
-			existing[name] = true
-		}
+	// collections, err := db.ListCollectionNames(ctx, bson.D{})
+	// if err == nil {
+	// 	existing := make(map[string]bool)
+	// 	for _, name := range collections {
+	// 		existing[name] = true
+	// 	}
 
-		for _, name := range global.MongoCollectionsToCreate {
-			if !existing[name] {
-				// Tạo mới collection một cách tường minh
-				err = db.CreateCollection(ctx, name)
-				if err != nil {
-					global.Logger.Error("Create MongoDB collection failed: ", zap.String("collection", name), zap.Error(err))
-				}
-			}
-		}
-	} else {
-		global.Logger.Error("List MongoDB collection names failed: ", zap.Error(err))
-	}
+	// 	for _, name := range global.MongoCollectionsToCreate {
+	// 		if !existing[name] {
+	// 			// Tạo mới collection một cách tường minh
+	// 			err = db.CreateCollection(ctx, name)
+	// 			if err != nil {
+	// 				global.Logger.Error("Create MongoDB collection failed: ", zap.String("collection", name), zap.Error(err))
+	// 			}
+	// 		}
+	// 	}
+	// } else {
+	// 	global.Logger.Error("List MongoDB collection names failed: ", zap.Error(err))
+	// }
 
 	global.Logger.Info("Init MongoDB connection success")
 }
